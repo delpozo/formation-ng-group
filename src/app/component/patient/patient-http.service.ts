58240a09412bebcd710430384244763ca2efbf0c
Virgil <virgilmeaup@hotmail.fr>
2020-07-08 17:32:17 +0200

    correction nom data

--- src/app/component/patient/patient-http.service.ts
+++ src/app/component/patient/patient-http.service.ts
@@ -19,29 +19,29 @@ export class PatientHttpService {
   }
 
   find(id: number): Observable<Patient> {
-    return this.http.get<Patient>("http://localhost:3000/petient/" + id);
+    return this.http.get<Patient>("http://localhost:3000/patient/" + id);
   }
 
   create(patient: Patient) {
-    return this.http.post<Patient>("http://localhost:3000/petient", patient).subscribe(resp => {
+    return this.http.post<Patient>("http://localhost:3000/patient", patient).subscribe(resp => {
       this.load()
     }, error => console.log(error));
   }
 
   update(patient: Patient) {
-    return this.http.put<Patient>("http://localhost:3000/petient/" + patient.id, patient).subscribe(resp => {
+    return this.http.put<Patient>("http://localhost:3000/patient/" + patient.id, patient).subscribe(resp => {
       this.load()
     }, error => console.log(error));
   }
 
   remove(id: number) {
-    return this.http.delete<void>("http://localhost:3000/petient/" + id).subscribe(resp => {
+    return this.http.delete<void>("http://localhost:3000/patient/" + id).subscribe(resp => {
       this.load()
     }, error => console.log(error));
   }
 
   load() {
-    this.http.get<Array<Patient>>("http://localhost:3000/petient").subscribe(resp => {
+    this.http.get<Array<Patient>>("http://localhost:3000/patient").subscribe(resp => {
       this.patients = resp;
     }, error => console.log(error));
   }
