0bb2981a4dc3a5d0514ce68c8a43876c7909169d
Virgil Moreau <virgilmeaup@hotmail.fr>
2020-07-08 16:48:55 +0200

    edition patient

---
+++ src/app/component/patient/patient-http.service.ts
@@ -0,0 +1,48 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {Patient} from "../../model/patient";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PatientHttpService {
+
+  patients: Array<Patient> = new Array<Patient>();
+
+  constructor(private http: HttpClient) {
+    this.load();
+  }
+
+  findAll(): Array<Patient> {
+    return this.patients;
+  }
+
+  find(id: number): Observable<Patient> {
+    return this.http.get<Patient>("http://localhost:3000/petient/" + id);
+  }
+
+  create(patient: Patient) {
+    return this.http.post<Patient>("http://localhost:3000/petient", patient).subscribe(resp => {
+      this.load()
+    }, error => console.log(error));
+  }
+
+  update(patient: Patient) {
+    return this.http.put<Patient>("http://localhost:3000/petient/" + patient.id, patient).subscribe(resp => {
+      this.load()
+    }, error => console.log(error));
+  }
+
+  remove(id: number) {
+    return this.http.delete<void>("http://localhost:3000/petient/" + id).subscribe(resp => {
+      this.load()
+    }, error => console.log(error));
+  }
+
+  load() {
+    this.http.get<Array<Patient>>("http://localhost:3000/petient").subscribe(resp => {
+      this.patients = resp;
+    }, error => console.log(error));
+  }
+}
