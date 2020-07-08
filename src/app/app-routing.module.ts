3f2b4ebdac50e57301e3bfa6bc0588c25430a47c
bochraab <bochra.bouajla@esprit.tn>
2020-07-08 17:18:01 +0200

    ModificationRoot Praticien

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,14 @@ import {HomeComponent} from './component/home/home.component';
 import {PatientComponent} from './component/patient/patient.component';
 import {DetailPatientComponent} from './component/detail-patient/detail-patient.component';
 import {LoginComponent} from './component/login/login.component';
+import {PraticienComponent} from "./praticien/praticien.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'patient', component: PatientComponent},
   {path: 'patient/:id', component: DetailPatientComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'praticien', component: PatientComponent},
+  {path: 'praticien', component: PraticienComponent},
   {path: 'praticien/:id', component: DetailPatientComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
