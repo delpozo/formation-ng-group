1f7c3144b036ba3b503caec36bc674e1adec449a
Skander Abdeljelil <skandar.abdeljelil@esprit.tn>
2020-07-08 12:39:48 +0200

    Ajout de la router-outlet dans le fichier app-component.html

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,20 +1,18 @@
 import {NgModule} from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
+import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from './component/home/home.component';
 import {PatientComponent} from './component/patient/patient.component';
 import {DetailPatientComponent} from './component/detail-patient/detail-patient.component';
 import {LoginComponent} from './component/login/login.component';
 
-
-
 const routes: Routes = [
-   {path: "home", component: HomeComponent},
-   {path: "patient", component: PatientComponent},
-   {path: "patient/:id", component: DetailPatientComponent},
-   {path: "login", component: LoginComponent},
-   {path: "praticien", component: PatientComponent},
-   {path: "praticien/:id", component: DetailPatientComponent},
-   {path: "", redirectTo: "home", pathMatch: "full"}
+  {path: 'home', component: HomeComponent},
+  {path: 'patient', component: PatientComponent},
+  {path: 'patient/:id', component: DetailPatientComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'praticien', component: PatientComponent},
+  {path: 'praticien/:id', component: DetailPatientComponent},
+  {path: '', redirectTo: 'home', pathMatch: 'full'}
 ];
 
 @NgModule({
