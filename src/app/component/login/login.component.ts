97bd1e607ad2b72e1e737c061524b3eecf7c3565
Saifeddine <sayftms@gmail.com>
2020-07-08 22:22:28 +0200

    login+db.json

--- src/app/component/login/login.component.ts
+++ src/app/component/login/login.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {LoginService} from '../../service/login.service';
+import {ID, LoginService, ROLE, USER} from '../../service/login.service';
 import {Router} from '@angular/router';
 import {User} from '../../model/user';
 
@@ -13,6 +13,11 @@ export class LoginComponent implements OnInit {
   username: string = '';
   password: string = '';
   role: string = 'admin';
+  Roles = [
+      { role:'admin'},
+      {role: 'patient'},
+      { role: 'praticien'}
+    ];
   user: User = null;
 
   constructor(private loginService: LoginService, private router: Router) {
@@ -24,19 +29,24 @@ export class LoginComponent implements OnInit {
   doLogin() {
     this.loginService.authenticate(this.username, this.password, this.role).subscribe(
       data => {
-        this.user = data;
-        console.log(JSON.stringify(data));
+        this.user = data[0];
+        console.log(data);
         console.log('userrrrrrrr = ' + this.user.username);
-        console.log('iddd = ' + this.loginService.getId());
-        // if (this.role === 'admin') {
-        //   this.loginService.findAdminByIdAndRole(this.user.id, this.role).subscribe(
-        //     data => {
-        //       console.log(JSON.stringify(data));
-        //       this.router.navigate(['/home']);
-        //     },
-        //     error => console.log(error)
-        //   );
-        // }
+        console.log('iddd = ' + this.user.id);
+
+        if (this.role === 'admin') {
+          this.router.navigate(['/home']);
+        } else
+          this.router.navigate(['/'+this.user.role]);
+        /*if (this.role === 'admin') {
+           this.loginService.findAdminByIdAndRole(this.user.id, this.role).subscribe(
+             data => {
+               console.log(JSON.stringify(data));
+               this.router.navigate(['/home']);
+             },
+             error => console.log(error)
+           );
+         }*/
       },
       error => console.log(error)
     );
